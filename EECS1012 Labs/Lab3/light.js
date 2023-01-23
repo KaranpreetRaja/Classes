bdf500ef83b3f270ca2f4134fab925f2ea9ad2ab
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-01-22 23:22:16 -0500

    Add lab3 Starting Code

---
+++ EECS1012 Labs/Lab3/light.js
@@ -0,0 +1,19 @@
+/* 22f Task 1 - write your code here */
+function onA(){
+
+ /* set image src to be that for light 1, and set message texts to be "light #1 is on"  */
+ 
+  
+}
+
+/* implement function onB(), onC(), onD(), onE() that are similar to onA() */
+function onB(){
+
+}
+
+....
+
+/* implement function oFF() for switching off the light*/
+function oFF(){
+    
+}
---
+++ EECS1012 Labs/Lab3/myLearningKit.js
@@ -0,0 +1,64 @@
+/* 22f 1012 JS files */
+function p01Func() {
+	
+  /* in Ex1, set message text and image sources for problem01
+  
+ 
+ 
+  /* the following three lines gets the checkboxes, and unchecks them
+  */
+  document.getElementById("check1").checked=false;
+  document.getElementById("check2").checked=false;
+  document.getElementById("check3").checked=false;
+
+  /* in Ex3, update display of images */
+
+
+}
+
+
+/* in Ex2, uncomment the following function and complete it*/
+
+/* remove this line
+function checkUncheck1(){
+  if (document.getElementById("???").checked==true) {
+     // add a getElementById here to get "flowchart" img and 
+     // set the display property of its style to one of "inline" or "none"	 
+
+	 
+  }
+  else {
+     // add a getElementById here to get "flowchart" img and 
+     // set the display property of its style to one of "inline" or "none"	 
+	  
+	  
+  }
+}
+remove this line */
+
+/* remove this line
+function checkUncheck2(){
+  if (document.getElementById("???").checked==true) {
+     // add a getElementById here to get "js" img and 
+     // set the display property of its style  
+	 
+	 
+  }
+  else {
+     // add a getElementById here to get "js" img and 
+     // set the display property of its style  
+	  
+	  
+  }
+}
+remove this line */
+
+/* in Ex2, you need to create function
+   checkUncheck3, which is similar to checkUncheck 1 and 2
+*/
+
+
+
+/* in Ex3, create function p02Func similar to p01Func */
+
+/* in Ex4, create functions zoomIn() and zoomOut() */
